933cede8b2474ed8fb866bbbabeb3662ae870db3
Justin Cho <jchoustin91@gmail.com>
2021-03-21 23:14:35 -0700

    Error Handler

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  //pass in error first in error middleware
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode; //Changing 200 into 500
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
 
@@ -19,6 +20,10 @@ app.get('/', (req, res) => {
 //3.  response object, call send ->
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
