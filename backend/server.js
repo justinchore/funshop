3d52024477b711824976334f2b66dba3a994f50d
Justin Cho <jchoustin91@gmail.com>
2021-03-25 17:20:42 -0700

    User Auth

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,15 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API IS RUNNING...');
@@ -20,6 +23,7 @@ app.get('/', (req, res) => {
 //3.  response object, call send ->
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
