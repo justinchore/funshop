8e77a75420657857fddc74e38e83b5a7a43c3d24
Justin Cho <jchoustin91@gmail.com>
2021-03-30 20:36:02 -0700

    Order Details Frontend Done

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -59,7 +59,7 @@ export const getOrderDetails = id => async (dispatch, getState) => {
       },
     };
 
-    const { data } = await axios.get(`/api/users/${id}`, config);
+    const { data } = await axios.get(`/api/orders/${id}`, config);
 
     dispatch({
       type: ORDER_DETAILS_SUCCESS,
