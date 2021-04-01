a738b31f4803165885058e73d398656dff6ecb9d
Justin Cho <jchoustin91@gmail.com>
2021-03-31 20:26:49 -0700

    Profile Screen Orders Frontend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -84,6 +84,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
+  console.log(req.user._id);
   const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 });
