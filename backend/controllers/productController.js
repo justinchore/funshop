13498abfcfb6ac8ad33085cb26a6db7c8cfce65f
Justin Cho <jchoustin91@gmail.com>
2021-04-02 17:19:24 -0700

    Product Create and Update Controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,4 +38,62 @@ const deleteProduct = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById, deleteProduct };
+// @desc    Create Product
+// @route   POST /api/products
+// @access  Private/ADMIN
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: 'Sample Name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    brand: 'Sample brand',
+    category: 'Sample category',
+    countInStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Updating Product
+// @route   PUT /api/products/:id
+// @access  Private/ADMIN
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+  const product = Product.findById(req.params.id);
+
+  if (product) {
+    (product.name = name),
+      (product.price = price),
+      (product.description = description),
+      (product.image = image),
+      (product.brand = brand),
+      (product.category = category),
+      (product.countInStock = countInStock);
+
+    const updatedProduct = await product.save();
+    res.status(201).json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error('Product does not exist');
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+};
